dd2ae5ed564431694352b9adaead432664bca424
gmco <gmco.magnet@gmail.com>
2025-01-30 14:02:52 -0600

    Update config.php

--- config.php
+++ config.php
@@ -9,6 +9,10 @@
 	$db_username = "u4bum5vo1sv2r2";
 	$db_password = "pe20a594001c2be5002cbb2aa26bc527b13edc6673e3e1376cd4dc6753ff89238";
 	
-	$db_conn = pg_connect($db_host, $db_name, $db_username, $db_password) or die("DB connection error");
+	$db_conn = pg_connect($db_host, $db_name, $db_username, $db_password);
+	if (!$db_conn) {
+		echo("DB Connection Failed <br>");
+		exit;
+	}
 
 ?>
