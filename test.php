106ba48ada146c477624ac763382bc63a94ebb87
gmco <gmco.magnet@gmail.com>
2025-01-30 13:25:28 -0600

    for deploy

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	$conn_string = "host=c8m0261h0c7idk.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com port=5432 dbname=dpe2kq3p3j0dv user=u4bum5vo1sv2r2 password=pe20a594001c2be5002cbb2aa26bc527b13edc6673e3e1376cd4dc6753ff89238";
-	$db_conn = pg_connect($conn_string);
-
-?>
