4d349e905d030fd0971b4111021ec3241ffb54dc
gmco <gmco.magnet@gmail.com>
2025-01-30 16:33:58 -0600

    pdo

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
 	echo("Test echo");
 	//echo(phpinfo());
 
+	/*
 	$db_host = "c8m0261h0c7idk.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
 	$db_port = "5432";
 	$db_name = "dpe2kq3p3j0dvdelete";
@@ -17,5 +18,18 @@
 	} else {
 		echo("DB Connection Successful");
 	}
+	*/
+
+	$db = parse_url(getenv("postgres://u4bum5vo1sv2r2:pe20a594001c2be5002cbb2aa26bc527b13edc6673e3e1376cd4dc6753ff89238@c8m0261h0c7idk.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dpe2kq3p3j0dv
+"));
+
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+    ltrim($db["path"], "/")
+));
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+    /*
     echo("<h1>Home Page</h1>");
 
     $db_host = "c8m0261h0c7idk.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
@@ -8,6 +9,9 @@
     $db_username = "u4bum5vo1sv2r2";
     $db_password = "pe20a594001c2be5002cbb2aa26bc527b13edc6673e3e1376cd4dc6753ff89238";
 
-    //$db_conn = pg_connect($db_host, $db_name, $db_username, $db_password);
-
+    $db_conn = pg_connect($db_host, $db_name, $db_username, $db_password);
+    */
+    
+    
+));
 ?>
