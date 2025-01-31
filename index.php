ed75c0d46b4d49e0db22ad6a2488b29e1121695f
gmco <gmco.magnet@gmail.com>
2025-01-31 15:05:00 -0600

    form page

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 
-echo("<h1>Home Page</h1>");
-
-/*
-    $db_host = "c8m0261h0c7idk.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
-    $db_port = "5432";
-    $db_name = "dpe2kq3p3j0dv";
-    $db_username = "u4bum5vo1sv2r2";
-    $db_password = "pe20a594001c2be5002cbb2aa26bc527b13edc6673e3e1376cd4dc6753ff89238";
-
-    $db_conn = pg_connect($db_host, $db_name, $db_username, $db_password);
-    */
-    
 ?>
+
+<html>
+    <head>
+        <script src="jquery-3.7.1.min.js"></script>
+        <script src="script.js"></script>
+    </head>
+    <body>
+    <h1>
+        Home Page
+    </h1>
+        <button id="goToFormPageBtn">Form</button>
+    </body>
+</html>
