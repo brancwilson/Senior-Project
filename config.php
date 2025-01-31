be9dbfe6603ef5ea480e352733ea1a7e64386f86
gmco <gmco.magnet@gmail.com>
2025-01-30 23:52:51 -0600

    Update config.php

--- config.php
+++ config.php
@@ -6,20 +6,9 @@
 	$db_host = "c8m0261h0c7idk.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
 	$db_port = "5432";
 	$db_name = "dpe2kq3p3j0dv";
-	$db_username = "u4bum5vo1sv2r2_delete";
+	$db_username = "u4bum5vo1sv2r2";
 	$db_password = "pe20a594001c2be5002cbb2aa26bc527b13edc6673e3e1376cd4dc6753ff89238";
 	
-
-	/*
-	$db_conn = pg_connect($db_host, $db_name, $db_username, $db_password);
-	echo($db_conn);
-	if (!$db_conn) {
-		echo("DB Connection Failed <br>");
-		exit;
-	} else {
-		echo("DB Connection Successful");
-	}
-	*/
 
 	try {
 		$dsn = "pgsql:host=$db_host;port=5432;dbname=$db_name;";
@@ -28,6 +17,11 @@
 	
 		if ($pdo) {
 			echo "Connected to the $db database successfully!";
+
+			$statement = $pdo->prepare('SELECT * FROM persons WHERE firstname=?');
+			$statement->execute([$firstname]);
+			$person = $statement->fetch();
+			echo($person);
 		}
 	} catch (PDOException $e) {
 		die($e->getMessage());
