bcd72778f2c6460c5cebe015ba99e900557437f2
gmco <gmco.magnet@gmail.com>
2025-02-01 15:47:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ $("document").ready(function() {
     });
 
     $("#newPersonSubmitBtn").click(function () {
+        console.log("New Person Button clicked...")
         $.ajax({
             url: 'formpage.php',
             type: 'POST',
             data: {fnInput: $("#fnInput").val(), lnInput:$("#lnInput").val()},
             success: function (result) {
                 alert('success');
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX error occured...");
             }
         });
     });
