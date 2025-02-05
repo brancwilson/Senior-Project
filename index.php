64e79a5ec92804834462d7bf8d8c17d709a71c36
gmco <gmco.magnet@gmail.com>
2025-02-05 17:56:38 -0600

    css

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
     <head>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="script.js"></script>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
     <h1>
         Home Page
     </h1>
-        <button id="goToFormPageBtn">Form</button>
+        <button id="goToFormPageBtn" class="optionBtn">Form</button>
     </body>
 </html>
