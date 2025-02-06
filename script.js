3f488893cbc5ffb2dc0455b61e95fb3f48eb097a
gmco <gmco.magnet@gmail.com>
2025-02-05 23:53:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -32,12 +32,16 @@ $("document").ready(function() {
             dataType: 'json',
             success: function(response) {
                 alert(response);
-            },
-            error: function(xhr, status, error) {
-                var err = JSON.parse(xhr.responseText);
-                alert(err.Message);
             }
         });
     });
 
 });
+
+/*
+,
+            error: function(xhr, status, error) {
+                var err = JSON.parse(xhr.responseText);
+                alert(err.Message);
+            }
+*/
