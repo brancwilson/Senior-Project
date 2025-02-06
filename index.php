d29e3648920f7e06d1cb6e856382937344096b60
gmco <gmco.magnet@gmail.com>
2025-02-05 18:04:47 -0600

    css

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="script.js"></script>
         <link rel="stylesheet" href="style.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Manjari:wght@100;400;700&display=swap" rel="stylesheet">
     </head>
     <body>
     <h1>
